2e47db3392d24d5b769c9b27fdc537e7eb6b89d0
SarahCaruthers <77997292+SarahCaruthers@users.noreply.github.com>
2021-05-01 13:31:48 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@
 using namespace std;
 
 int main(){
-  BST<int> test;
-  test.insert(1);
-  test.insert(2);
-  test.insert(3);
-  //test.printInOrder();
-  test.deleteValue(2);
+  BST<Faculty> test;
+  Faculty f("Sarah", 1, "Soph", "crean");
+  test.insert(f);
+  f.addAdvisee(12);
+  f.addAdvisee(13);
+  f.addAdvisee(14);
+  Faculty f2("Sophia", 2, "Freshman", "fowler");
+  test.insert(f2);
+  f2.addAdvisee(15);
+  f2.addAdvisee(16);
   test.printInOrder();
   return 0;
 }
