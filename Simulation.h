b97afa81174b995dc221fee0fc49554ace397109
SarahCaruthers <77997292+SarahCaruthers@users.noreply.github.com>
2021-05-03 20:26:14 -0700

    Update Simulation.h

--- Simulation.h
+++ Simulation.h
@@ -1,3 +1,4 @@
+
 #ifndef SIMULATION_H
 #define SIMULATION_H
 
@@ -5,6 +6,7 @@ using namespace std;
 #include "Student.h"
 #include "Faculty.h"
 #include "BST.h"
+//#include "CirQ.h"
 
 // need to write general find node alg to find all the people
 class Simulation{
@@ -20,18 +22,19 @@ public:
   void printFacultyAscending();
   void printStudentGivenID(int iD);
   void printFacultyGivenID(int iD);
-  void printFacGivenStudentID(BST<Student>* goldStudent, BST<Faculty>* goldFaculty, int iD); // find student, store faculty name. FInd fac in facTree and printInfo()
-  void printFacAdviseeList(BST<Student>* goldStudent, BST<Faculty>* goldFaculty, int iD); // open loop on facTree, store current ID in variable, traverse goldStudent for ID and printInfo()
+  void printFacGivenStudentID(int iD); // find student, store faculty name. FInd fac in facTree and printInfo()
+  void printFacAdviseeList(int iD); // open loop on facTree, store current ID in variable, traverse goldStudent for ID and printInfo()
   void deleteStudent(int iD); // call removeFrom()
   void deleteFaculty(int iD);
-  void changeStudentAdvisor(int studentID, int facultyID, BST<Student>* goldStudent, BST<Faculty>* goldFaculty); // update student.setFacAdvisID() with new ID
-  void deleteAdvisee(int sID); // move method from faculty class here
+  void changeStudentAdvisor(int studentID, int facultyID); // update student.setFacAdvisID() with new ID
+  void deleteAdvisee(int studentID, int facultyID); // move method from faculty class here
 
 private:
   BST<Student>* goldStudent;
   BST<Faculty>* goldFaculty;
-  //BST<Student>* studArr[5];
-  //BST<Faculty> facArr[5];
+  DLList<int>* advisee;
+  // CirQ<BST<Student>>* studQ;
+  // CirQ<BST<Faculty>>* facQ;
 
 };
 
